12847678a2b3a021b505bfd8a150a9a3fb686022
William Leith <williamleith@Williams-MBP.cable.rcn.com>
2022-04-03 17:22:11 -0400

    reverting

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ import helloController  from "./controllers/hello-controller.js";
 import userController   from "./controllers/user-controller.js";
 import tuitsController  from "./controllers/tuits-controller.js";
 const app = express();
-app.use(cors())
-// app.use(express.json());
+app.use(cors());
+app.use(express.json());
 helloController(app);
 userController(app);
 tuitsController(app);
