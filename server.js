70ca4c7862073cc66dadba1f61376bdbe78b2724
William Leith <williamleith@Williams-MBP.cable.rcn.com>
2022-04-03 16:37:42 -0400

    adding access-control-allow-origin to response headers

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ import helloController  from "./controllers/hello-controller.js";
 import userController   from "./controllers/user-controller.js";
 import tuitsController  from "./controllers/tuits-controller.js";
 const app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(cors());
 app.use(express.json());
 helloController(app);
